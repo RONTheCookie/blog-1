cba2674db521b8423dd82371b8617719ef5bf1bc
jellz <danielgulic@gmail.com>
2019-01-28 17:44:32 +1100

    new post schema

--- server/src/routes/posts.js
+++ server/src/routes/posts.js
@@ -1,6 +1,12 @@
 var express = require('express'),
-    { r } = require('../index.js');
+    { r } = require('../index.js'),
+    Joi = require('joi');
 var router = express.Router();
+
+var newPostRequest = Joi.object().keys({
+  title: Joi.string().max(45).min(1).required(),
+  content: Joi.string().max(50000).required()
+}).required();
 
 router.get('/', async (req, res) => {
   var limit = req.query.limit || 10;
